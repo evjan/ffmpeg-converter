73794eac2fea44a1f23d685631c93c1351d97547
Peter Evjangst <hello@peterevjan.com>
2021-03-14 15:14:32 +1100

    Switching to ProRes HQ and Proxy by default

--- app.rb
+++ app.rb
@@ -1,24 +1,21 @@
 require 'json'
 require 'fileutils'
 
-Dir.chdir('raw')
 all_files = Dir.glob('**/*.MP4');
-
-Dir.chdir('..')
 
 proxy = ARGV[0] == "--proxy"
 
 output_dir = ""
-quality = ""
+profile = 0
 
 if(proxy)
   puts "Generating proxies..."
   output_dir = "converted/proxies"
-  quality = '"scale=1920:1080p,fps=50,format=yuv422p" -b:v 36M'
+  profile = 0 # proxy
 else
   puts "Generating HQ videos..."
   output_dir = "converted/hq"
-  quality = '"scale=1920:1080p,fps=50,format=yuv422p10" -b:v 440M'
+  profile = 3 # hq
 end
 
 FileUtils.rm_rf output_dir
@@ -26,9 +23,8 @@ FileUtils.mkdir_p output_dir
 
 all_files.each do |f|
   puts "Converting #{f}"
-  info = JSON.parse(`ffprobe -v quiet -print_format json -show_streams -show_format "#{f}"`)
   dirname = f.split('/')[0...-1].join("/")
   puts "DIRNAME: #{dirname}"
   FileUtils.mkdir_p("#{output_dir}/#{dirname}")
-  `ffmpeg -i "raw/#{f}" -vf #{quality} -c:a pcm_s16be -c:v dnxhd "#{output_dir}/#{f}.mov"`
+  `ffmpeg -i "#{f}" -c:v prores_ks -profile:v #{profile} -vendor apl0 -pix_fmt yuv422p10le "#{output_dir}/#{f}.mov"`
 end
